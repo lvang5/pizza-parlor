61b70744ac56bd84197908580317eba429b54f20
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-09-06 15:48:15 -0500

    axios GET route completed and add and delete buttons appearing on DOM.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-export default class Home extends Component {
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    // this.state = {
+
+    // }
+  }
+
+  componentDidMount() {
+    this.getPizzas();
+  }
+
+  // axios GET all of the pizzas available for selection -- map over pizzas
+  getPizzas() {
+    axios({
+      method: 'GET',
+      url: '/'
+    }).then((response) => {
+      const pizzas = response.data; // Array of pizza selections
+      const action = { type: 'ADD_ORDER', payload: pizzas };
+      this.props.dispatch(action);
+    })
+  }
+  // Reducer to track order add or remove 
+
   render() {
     return (
       <div>
-        
+        <h3>Step 1: Select Your Pizza</h3>
+
+        <div className = "card">
+        <img src = "images/pizza_photo.png"/>
+        <br /> <button>Add</button>
+        <button>Remove</button>
+        </div>
+
+
       </div>
     )
   }
-}
+
+
+
+} // end Component
+
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState
+});
+export default connect(mapReduxStateToProps)(Home);
