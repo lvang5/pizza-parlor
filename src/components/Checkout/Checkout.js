f359babb3134100ce682170e5bc704a2966735bb
Tr4vi5 <trav.dunn@outlook.com>
2018-09-06 15:28:53 -0500

    created the skeleton of checkout componenet and deleted pizza image

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -4,7 +4,27 @@ export default class Checkout extends Component {
   render() {
     return (
       <div>
-        
+        <h2>Step 3: Checkout</h2>
+        <div>
+            <ul>
+                <li>Name ph</li>
+                <li>Street ph</li>
+                <li>City zip ph</li>
+            </ul>
+            <h3>Type placeholder</h3>
+        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Cost</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* map over array of pizzas in customer's cart */}
+            </tbody>
+        </table>
+        <h1>Total:</h1>
       </div>
     )
   }
